1f74c4ab990de04be729580dc96e51492fea246b
Lewis Benson <tm.lewisbenson@gmail.com>
2022-12-16 00:52:29 -0500

    populate test files for deployment

---
+++ client/clientHandler.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+
+
+
+describe('client unit tests', () => {
+  it('receives a message when an issue is resolved', () => {
+    expect(() => {
+      'application'.toBeNull();
+    });
+  });
+});
